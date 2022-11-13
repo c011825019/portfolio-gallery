5757538392ffda0db77d265e9ec45645d320c833
c011825019 <112187295+c011825019@users.noreply.github.com>
2022-11-13 17:05:32 +0000

    Add ユーザー編集・削除機能

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -8,7 +8,18 @@ class Public::UsersController < ApplicationController
   end
 
   def update
-    
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to my_page_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @user.destroy
+    redirect_to root_path
   end
 
   private
