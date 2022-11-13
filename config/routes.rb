92e7e0941df37bbf861b0fe067b7cad717482a7e
c011825019 <112187295+c011825019@users.noreply.github.com>
2022-11-13 14:15:14 +0000

    Add ユーザーログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,19 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
-  
+
+  namespace :admin do
+    resources :users, only: [:index, :show, :edit, :update]
+  end
+
+  scope module: :public do
+    root to: "homes#top"
+
+    get 'my_page' => 'users#show'
+    get 'information/edit' => 'users#edit'
+    patch 'information' => 'users#update'
+    delete 'information/destroy' => 'users#destroy'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
