f47c1e49ab0fd627dac67b191aa4961b6e58ab0d
c011825019 <112187295+c011825019@users.noreply.github.com>
2022-11-22 17:57:17 +0000

    Add バリデーション設定

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :portfolio
+
+  validates :comment, length: { minimum: 1, maximum: 300 }, presence: true
+  validates :evaluation, presence: true
 end
