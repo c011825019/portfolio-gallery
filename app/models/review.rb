85d052d0480acf613595b974791786e6d0634f54
c011825019 <112187295+c011825019@users.noreply.github.com>
2022-11-23 03:13:24 +0900

    Revert "Add バリデーション設定"

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :portfolio
-
-  validates :comment, length: { minimum: 1, maximum: 300 }, presence: true
-  validates :evaluation, presence: true
 end
