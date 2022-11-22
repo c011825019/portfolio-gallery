2c4bce6fdca88b7d6dac472acb365b32c8dbef01
c011825019 <112187295+c011825019@users.noreply.github.com>
2022-11-22 18:48:00 +0000

    Add バリデーションの設定

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :portfolio
+
+  validates :comment, length: { maximum: 300 }, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
   has_many :portfolios, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 300 }
+
   def get_profile_image
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
