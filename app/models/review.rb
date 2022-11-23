8aa8d64dfa59698d3a1f2ede1741c9d5f33ed436
c011825019 <112187295+c011825019@users.noreply.github.com>
2022-11-23 16:34:34 +0000

    Fix レビュー一覧評価表示

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :portfolio
 
+  validates :evaluation, presence: true
   validates :comment, length: { maximum: 300 }, presence: true
 end
